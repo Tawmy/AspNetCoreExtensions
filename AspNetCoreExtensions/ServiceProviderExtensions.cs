bfbeffd972593f436c981c0401c30597e3999cb0
Tawmy <git@tawmy.dev>
2025-04-07 11:21:17 +0200

    Add ServiceProviderExtensions
    Allows for database migration on startup

---
+++ AspNetCoreExtensions/ServiceProviderExtensions.cs
@@ -0,0 +1,37 @@
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace AspNetCoreExtensions;
+
+public static class ServiceProviderExtensions
+{
+    public static async Task MigrateDatabaseAsync<T>(this IServiceProvider serviceProvider) where T : DbContext
+    {
+        using var scope = serviceProvider.CreateScope();
+
+        var dbContext = scope.ServiceProvider.GetRequiredService<T>();
+
+        await dbContext.Database.MigrateAsync();
+
+        // Reload Npgsql types
+        // https://github.com/npgsql/efcore.pg/issues/292#issuecomment-1829713529
+        if (dbContext.Database.GetDbConnection() is NpgsqlConnection npgsqlConnection)
+        {
+            if (npgsqlConnection.State != ConnectionState.Open)
+            {
+                await npgsqlConnection.OpenAsync();
+            }
+
+            try
+            {
+                await npgsqlConnection.ReloadTypesAsync();
+            }
+            finally
+            {
+                await npgsqlConnection.CloseAsync();
+            }
+        }
+    }
+}
