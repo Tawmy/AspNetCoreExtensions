dcc0e5bd897367b0f05bcc2ce47c6bef726cac4e
Tawmy <git@tawmy.dev>
2025-12-20 13:05:27 +0100

    Add support for client credentials, custom scopes

---
+++ AspNetCoreExtensions/ClientCredentialsExtensions.cs
@@ -0,0 +1,38 @@
+using AspNetCoreExtensions.Keycloak;
+using Duende.AccessTokenManagement;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreExtensions;
+
+public static class ClientCredentialsExtensions
+{
+    extension(IServiceCollection services)
+    {
+        public void AddKeycloakClientCredentials(KeycloakConfiguration idp)
+        {
+            services.AddSingleton<ClientAssertionService>(_ =>
+                new ClientAssertionService(idp.Authority, idp.ClientId,
+                    idp.CertificatePath, idp.PrivateKeyPath));
+
+            services.AddClientCredentialsTokenManagement()
+                .AddClient(ClientCredentialsClientName.Parse(idp.ClientId), x =>
+                {
+                    // TODO is there a way to do this without querying the authority URL?
+                    x.TokenEndpoint = new Uri($"{idp.Authority}/protocol/openid-connect/token");
+
+                    x.ClientId = ClientId.Parse(idp.ClientId);
+                    x.Scope = Scope.Parse(string.Join(' ', idp.Scopes));
+
+                    if (idp.ClientSecret is not null)
+                    {
+                        x.ClientSecret = ClientSecret.Parse(idp.ClientSecret);
+                    }
+                });
+
+            if (idp.CertificatePath is not null)
+            {
+                services.AddSingleton<ITokenRequestCustomizer, SignedJwtRequestCustomizer>();
+            }
+        }
+    }
+}
--- AspNetCoreExtensions/Keycloak/KeycloakConfiguration.cs
+++ AspNetCoreExtensions/Keycloak/KeycloakConfiguration.cs
@@ -11,6 +11,7 @@ public record KeycloakConfiguration
     public string Authority { get; private init; }
     public string ClientId { get; private init; }
     public string? ClientSecret { get; private init; }
+    public IEnumerable<string> Scopes { get; private init; } = [];
     public string? CertificatePath { get; private init; }
     public string? PrivateKeyPath { get; private init; }
 
@@ -20,9 +21,15 @@ public record KeycloakConfiguration
     /// <param name="authority">OpenID Connect Authority (realm URL)</param>
     /// <param name="clientId">OpenID Connect Client ID</param>
     /// <param name="clientSecret">OpenID Connect Client Secret</param>
-    public static KeycloakConfiguration WithClientSecret(string authority, string clientId, string clientSecret)
+    /// <param name="scopes">Optional list of scopes to submit to Keycloak</param>
+    public static KeycloakConfiguration WithClientSecret(string authority, string clientId, string clientSecret,
+        params IEnumerable<string> scopes)
     {
-        return new KeycloakConfiguration(authority, clientId) { ClientSecret = clientSecret };
+        return new KeycloakConfiguration(authority, clientId)
+        {
+            ClientSecret = clientSecret,
+            Scopes = scopes
+        };
     }
 
     /// <summary>
@@ -30,15 +37,17 @@ public record KeycloakConfiguration
     /// </summary>
     /// <param name="authority">OpenID Connect Authority (realm URL)</param>
     /// <param name="clientId">OpenID Connect Client ID</param>
-    /// <param name="certificatePath">Path to PEM-formatted certificate to use for JWKS endpoint.</param>
-    /// <param name="privateKeyPath">Path to PEM-formatted private key to use for Signed JWT client authentication.</param>
+    /// <param name="certificatePath">Path to PEM-formatted certificate to use for JWKS endpoint</param>
+    /// <param name="privateKeyPath">Path to PEM-formatted private key to use for Signed JWT client authentication</param>
+    /// <param name="scopes">Optional list of scopes to submit to Keycloak</param>
     public static KeycloakConfiguration WithSignedJwt(string authority, string clientId, string certificatePath,
-        string privateKeyPath)
+        string privateKeyPath, params IEnumerable<string> scopes)
     {
         return new KeycloakConfiguration(authority, clientId)
         {
             CertificatePath = certificatePath,
-            PrivateKeyPath = privateKeyPath
+            PrivateKeyPath = privateKeyPath,
+            Scopes = scopes
         };
     }
 }
