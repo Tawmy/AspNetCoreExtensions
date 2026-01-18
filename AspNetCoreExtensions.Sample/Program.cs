5b5983fb5e8bccd43127e202235895530fb6c5bc
Tawmy <git@tawmy.dev>
2026-01-18 13:29:09 +0100

    Add GitHub workflow to push container image

--- AspNetCoreExtensions.Sample/Program.cs
+++ AspNetCoreExtensions.Sample/Program.cs
@@ -1,4 +1,5 @@
 using AspNetCoreExtensions.Sample.Components;
+using Tailwind;
 using _Imports = AspNetCoreExtensions.Sample.Client._Imports;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.UseTailwindCli();
 
 var app = builder.Build();
 
